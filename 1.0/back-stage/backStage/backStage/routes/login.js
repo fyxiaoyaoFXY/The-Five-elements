4f85c78f65483f5e6671eaa44de83f619acad218
houdesen <zh06101019>
2019-12-06 18:23:00 +0800

    backStage1.0.0

--- 1.0/back-stage/backStage/backStage/routes/login.js
+++ 1.0/back-stage/backStage/backStage/routes/login.js
@@ -1,49 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require("mysql");
+var dbconfig = require("../config/dbconfig.json");
 
 router.get('/', function(req, res, next) {
   res.render('login');
 });
 
-router.post('/add', function(req, res, next) {
-  // console.log(req.body);
-  var title = req.body.title;
-  var content = req.body.content;
+router.post('/login', function(req, res, next) {
+  // console.log(req);
+  var username = req.body.username;
+  var password = req.body.password;
   var con = mysql.createConnection(dbconfig);
   con.connect();
-  con.query("insert into chapters(title,content) values(?,?)",[title,content],function(err,result){
+  con.query("select username,password from user",function(err,result){
     if(err){
       console.log(err);
     } else {
       console.log(result);
-      res.end("insert success");
+      for(var i = 0; i < result.length; i++){
+        if(username == result[i].username && password == result[i].password){
+          res.end('success');
+        }
+        if(username == '' || password == ''){
+          res.end('用户名，密码为空');
+        }
+      }
+      res.end('lost');
     }
   })
 });
 
-router.get("/list",function(req,res,next){
-  var con = mysql.createConnection(dbconfig);
-  con.connect();
-  con.query("select * from chapters",function(err,result){
-    if(err){
-      console.log(err);
-    } else {
-      // console.log(result);
-      res.render("list",{chapterList:result});
-    }
-  })
-});
-
-router.get("/del",function(req,res,next){
-  var chapterId = req.query.chapterid;
-  var con = mysql.createConnection(dbconfig);
-  con.connect();
-  con.query("delete from chapters where chapterid=?",[chapterId],function(err,result){
-    if(err){
-      console.log(err);
-    } else {
-      console.log('delete success');
-    }
-  })
-})
 module.exports = router;
